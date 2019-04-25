a71595c9b621460dcc9ec2a788cc89d3d073a8ec
Sokkun.Norn <Sokkun.Norn@PNC.LAN>
2019-04-25 08:16:02 +0700

    [S001] this is template selection committee
    refs S001

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,34 +1,60 @@
+</div>
+</div>
+<!-- end: Content -->
 
-        <div class="clearfix"></div>
-        <!-- Footer -->
-        <footer class="site-footer">
-                <div class="footer-inner bg-white">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            Copyright &copy; Wep_2019
-                        </div>
-                        <div class="col-sm-6 text-right">
-                            Designed by VC2_groupB
-                        </div>
-                    </div>
-                </div>
-            </footer>
-            <!-- /.site-footer -->
-        </div>
-        <!-- /#right-panel -->
+</div>
 
-        <!-- Scripts -->
-        <script src="{{asset('js/app.js')}}"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-        <script src="{{asset('js/main.js')}}"></script>
-        <!--  Chart js -->
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.3/dist/Chart.bundle.min.js"></script>
-        <script src="{{asset('js/chartjs-init.js')}}"></script>
-        <!--Flot Chart-->
-        <script src="https://cdn.jsdelivr.net/npm/jquery.flot@0.8.3/jquery.flot.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/flot-spline@0.0.1/js/jquery.flot.spline.min.js"></script>
+<!-- start: Mobile -->
+<div id="mimin-mobile" class="reverse">
+<div class="mimin-mobile-menu-list">
+  <div class="col-md-12 sub-mimin-mobile-menu-list animated fadeInLeft">
+      <ul class="nav nav-list">
+          <li class="active ripple">
+            <a class="tree-toggle nav-header">
+              <span class="fa-graduation-cap fa"></span>Candidate 
+              <span class="fa-angle-right fa right-arrow text-right"></span>
+            </a>
+            <ul class="nav nav-list tree">
+                <li><a href="dashboard-v1.html">List Candidates</a></li>
+                <li><a href="dashboard-v2.html">Add Candidate</a></li>
+            </ul>
+          </li>
+          <li class="ripple">
+            <a class="tree-toggle nav-header">
+              <span class="fa-users fa"></span>User
+              <span class="fa-angle-right fa right-arrow text-right"></span>
+            </a>
+            <ul class="nav nav-list tree">
+              <li><a href="topnav.html">List Users</a></li>
+              <li><a href="boxed.html">Add User</a></li>
+            </ul>
+          </li>
+      </ul>
+  </div>
+</div>       
+</div>
+<button id="mimin-mobile-menu-opener" class="animated rubberBand btn btn-circle btn-danger">
+<span class="fa fa-bars"></span>
+</button>
+<!-- end: Mobile -->
+
+<!-- start: Javascript -->
+
+    <script src="{{asset('asset/js/jquery.min.js')}}"></script>
+    <script src="{{asset('asset/js/jquery.ui.min.js')}}"></script>
+    <script src="{{asset('asset/js/bootstrap.min.js')}}"></script>
+
+    <script src="{{asset('asset/js/plugins/moment.min.js')}}"></script>
+    <script src="{{asset('asset/js/plugins/flot/jquery.flot.min.js')}}"></script>
+    <script src="{{asset('asset/js/plugins/flot/jquery.flot.pie.min.js')}}"></script>
+    <script src="{{asset('asset/js/plugins/flot/jquery.flot.time.min.js')}}"></script>
+    <script src="{{asset('asset/js/plugins/flot/jquery.flot.navigate.min.js')}}"></script>
+    <script src="{{asset('asset/js/plugins/flot/jquery.flot.stack.min.js')}}"></script>
+
+    <script src="{{asset('asset/js/plugins/jquery.nicescroll.js')}}"></script>
+
+    <script src="{{asset('asset/js/main.js')}}"></script>
+
     </body>
 </html>
 
---
+++ resources/views/layout/leftSideBar.blade.php
@@ -0,0 +1,33 @@
+
+<!-- start:Left Menu -->
+<div id="left-menu">
+    <div class="sub-left-menu scroll">
+      <ul class="nav nav-list">
+          <li><div class="left-bg"></div></li>
+          <li class="time">
+            <h1 class="animated fadeInLeft">21:00</h1>
+            <p class="animated fadeInRight">Sat,October 1st 2029</p>
+          </li>
+          <li class="active ripple">
+            <a class="tree-toggle nav-header"><span class="fa fa-graduation-cap"></span> Candidate 
+              <span class="fa-angle-right fa right-arrow text-right"></span>
+            </a>
+            <ul class="nav nav-list tree">
+                <li><a href="dashboard-v1.html">List Candidates</a></li>
+                <li><a href="dashboard-v2.html">Add Candidate</a></li>
+            </ul>
+          </li>
+          <li class="ripple">
+            <a class="tree-toggle nav-header">
+              <span class="fa-users fa"></span> User
+              <span class="fa-angle-right fa right-arrow text-right"></span>
+            </a>
+            <ul class="nav nav-list tree">
+              <li><a href="topnav.html">List Users</a></li>
+              <li><a href="boxed.html">Create User</a></li>
+            </ul>
+          </li>
+      </ul>
+    </div>
+</div>
+<!-- end: Left Menu -->
